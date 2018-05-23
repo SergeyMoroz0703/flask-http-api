16d046d61355dcb1fe90024c2e1518b1d3b7339f
Serhii Moroz <smoroz@atlassian.com>
2018-05-23 18:47:07 +0300

    test+

--- unittest.py
+++ unittest.py
@@ -3,3 +3,15 @@ import unittest
 
 DEFAULT_HEADER = 'application/json'
 USER_TO_CREATE = {'name': 'James', 'surname': 'Bond', 'online': False}
+
+
+
+class TestApiFunctionality(unittest.TestCase):
+
+    def __init__(self, *a, **kw):
+        super(TestApiFunctionality, self).__init__(*a, **kw)
+        self.host = '127.0.0.1:5000'
+        self.db_url = 'users'
+        self.url = 'http://{}/{}'.format(self.host, self.db_url)
+
+    def post_user(self):
